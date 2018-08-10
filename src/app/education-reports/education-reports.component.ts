d08cb6c450401660ff690dbae69a3ef06791df60
Ian Highsmith <ianhighsmith@Ians-iMac.local>
2018-08-10 14:00:12 -0400

    component background imagesx

--- src/app/education-reports/education-reports.component.ts
+++ src/app/education-reports/education-reports.component.ts
@@ -8,7 +8,7 @@ import { EducationReportService } from './education-report.service';
   providers: [EducationReportService]
 })
 export class EducationReportsComponent implements OnInit {
-
+  imgSrc = 'http://epmgaa.media.clients.ellingtoncms.com/img/photos/2016/06/23/College_campus_t750x550.jpg?d885fc46c41745b3b5de550c70336c1b382931d2';
   constructor() { }
 
   ngOnInit() {
