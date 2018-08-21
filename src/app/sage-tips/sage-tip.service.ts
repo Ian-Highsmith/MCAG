64df039a11a332cffa21593172e5c1f995e0e66d
Ian Highsmith <ianhighsmith@Ians-iMac.local>
2018-08-21 11:32:57 -0400

    footer added

--- src/app/sage-tips/sage-tip.service.ts
+++ src/app/sage-tips/sage-tip.service.ts
@@ -6,9 +6,13 @@ export class SageTipService {
 
   private sageTips: SageTip[] = [
     new SageTip('SAGE TIP 1', 'How To Save For College How To Save For College How To Save For College How To Save For College How To Save For College How To Save For College How To Save For College',
-  'https://previews.123rf.com/images/akiradesigns/akiradesigns1310/akiradesigns131000083/22528727-cartoon-vector-illustration-of-porr-man-and-woman-thinking-about-money.jpg'),
+    'https://previews.123rf.com/images/akiradesigns/akiradesigns1310/akiradesigns131000083/22528727-cartoon-vector-illustration-of-porr-man-and-woman-thinking-about-money.jpg'),
+    new SageTip('SAGE TIP 1', 'How To Save For College How To Save For College How To Save For College How To Save For College How To Save For College How To Save For College How To Save For College',
+    'https://previews.123rf.com/images/akiradesigns/akiradesigns1310/akiradesigns131000083/22528727-cartoon-vector-illustration-of-porr-man-and-woman-thinking-about-money.jpg'),
+    new SageTip('SAGE TIP 1', 'How To Save For College How To Save For College How To Save For College How To Save For College How To Save For College How To Save For College How To Save For College',
+    'http://tadalafilforsale.net/data/media/90/61593586.jpg'),
     new SageTip('SAGE TIP 2', 'How To Choose Your Dream School',
-  'http://tadalafilforsale.net/data/media/90/61593586.jpg')
+    'http://tadalafilforsale.net/data/media/90/61593586.jpg')
   ];
 
   constructor() {}
