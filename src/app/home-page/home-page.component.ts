cc09b8f63582b4bb9eddef4721de3578ebf04157
Ian Highsmith <ianhighsmith@Ians-iMac.local>
2018-08-28 10:28:29 -0400

    footer changes

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -8,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageComponent implements OnInit {
 
   constructor() {}
-  
+
   ngOnInit() {
   }
 }
